994b9e1753755c4ad898ab1d9150fc1f1015579e
evan <stewarea@gmail.com>
2016-03-02 18:43:50 -0800

    add framework for html and js

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var tritype = function(side1, side2, side3) {
+  if ((side1 === side2) && (side1 === side3)) {
+        return "equilateral"
+} else if ((side1 === side2) || (side2 === side3) || (side1 === side3)) {
+        return "ioceles"
+} else if ((side1 + side2 > side3) || (side1 + side3 > side2) || (side2 + side3 > side1)) {
+        return "scalene"
+} else   {
+        return "this is not a triangle"
+  }
+}
+
+$(document).ready(function() {
+  $("form#triangles").submit(function(event) {
+
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var result = tritype(side1, side2, side3);
+
+    $(".tritype").text(result);
+
+    $("#result").show();
+
+    event.preventDefault();
+
+  })
+});
